3e143a23efa32c4f9d1e13e1bf12235065702c20
patrickcgray <patrickcgray@github.com>
2015-03-31 14:48:16 -0400

    wordnik is now working and basic functionality it all good

--- chatbot.py
+++ chatbot.py
@@ -11,7 +11,7 @@ from wordnik import *
 ### starting wordnik API ###
 
 apiUrl = 'http://api.wordnik.com/v4'
-apiKey = '0'
+apiKey = 'f4581649e01134254d6c3259d6607bfb6c3ef144f0deba6ec'
 client = swagger.ApiClient(apiKey, apiUrl)
 wordApi = WordApi.WordApi(client)
 
@@ -26,9 +26,6 @@ class Minerva(object):
         self.interests = []
         self.disinterests = []
         self.user_memory_dict = {1: 'first user memory', 2: 'second user memory'}
-
-    def method_a(self, foo):
-        print (self.x + ' ' + foo)
 
     def choose_interest(self):
         if self.interests:
@@ -69,12 +66,12 @@ def build_noun_list(input_string):
     tokens = nltk.word_tokenize(input_string)
     tagged = nltk.pos_tag(tokens)
     nouns = []
-    print (tagged)
+    #print (tagged)
     count = 1
     for item in tagged:
         if item[1] == 'NN' or item[1] == 'NNS':
-            print ('item[0] is ')
-            print (str(count) + 'noun is: ')
+            #print ('item[0] is ')
+            #print (str(count) + 'noun is: ')
             count += 1
             nouns.append(item[0])
     return nouns
@@ -86,39 +83,43 @@ def stripAllTags( html ):
 
 ###
 
-### primary function to run the program ###
+### primary function to run Miss M ###
 
 def run_minerva():
-    print ('Ask or tell me something por favor')
-    print ('*** Just type "exit" in order to quit')
+    print ('*** Just type "goodbye" in order to quit\n')
+    print ('Hello there! What is on your mind?\n')
     while (1<2):
-        user_input = input("Type here: ")
-        if user_input == 'exit':
+        user_input = input()
+        if user_input == 'goodbye' or user_input == 'bye':
+            print("So long! I'll miss you!")
             break
         else:
             nouns = build_noun_list(user_input)
-            print ('')
-            print ('the nouns list')
-            print (nouns)
-            print ('')
-            print ('you used ' + str(len(nouns)) + ' nouns')
+            #print ('')
+            #print ('the nouns list')
+            #print (nouns)
+            #print ('')
+            #print ('you used ' + str(len(nouns)) + ' nouns')
             if len(nouns) > 0:
                 #soup = soupify_url('http://dictionary.reference.com/browse/' + nouns[0]) 
                 #dndata_tags = soup.findAll('div',{'class':'dndata'})
                 #print(dndata_tags)
                 definitions = wordApi.getDefinitions(nouns[0])
                 def1 = definitions[0].text
-                text = dndata_tags[0]
-                print ('I find ' + nouns[0] + ' most interesting. Did you know that they are ' + re.sub('<[^<]+?>', '', str(def1)))
-                
-            if 'who' or 'what' or 'when' or 'where' or 'why' or 'how' in user_input:
-                print ('you asked a question')
-            
-            print ('you said: ' + user_input)
+                #text = dndata_tags[0]
+                print ('Did you know that ' + nouns[0] + ' is ' + str(def1))
+                print('')
+                example_results = wordApi.getExamples(nouns[0])
+                chosen_example = random.choice(example_results.examples)
+                print(chosen_example.text)
+                print('')
+            else:
+                print('Oh yeah? Tell me more!\n')
+            #print ('you said: ' + user_input)
 
 ###
 
-### calling the program ###
+### calling Madam M ###
 
 def main(argv=None):
     run_minerva()
